308b32e78f466af859fd31c382f4b1bb12ebbc48
Kira <kira.soba@gmx.de>
2023-01-04 21:41:58 +0100

    Feat: adding cards to column

--- src/components/Collumn/Collumn.jsx
+++ src/components/Collumn/Collumn.jsx
@@ -1,12 +1,29 @@
 import './collumn.scss'
 import Card from '../Card/Card'
+import { useEffect, useState } from 'react'
 
 export default function Collumn() {
+    const [cards, setCards] = useState([])
+
+    useEffect(() => {
+        console.log("rendering")
+    }, [cards])
+
+    function addCard() {
+        console.log('Button clicked')
+        setCards([...cards, { title: `card ${cards.length + 1}`, description: "a description" }])
+    }
+
+    console.log(cards)
+
     return (
         <div className="collumn">
             <h2>Column Title</h2>
             <Card />
-            <button>Add Card</button>
+            {cards.map((card, index) => {
+                return <Card key={index} />
+            })}
+            <button onClick={addCard}>Add Card</button>
         </div>
     )
 }
