71ccd2e3edf7a720cb5c0097341c1ce6bfb1004b
Kira <kira.soba@gmx.de>
2023-01-04 21:06:43 +0100

    Components for collumn and card with basic layout

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,10 @@
+import './card.scss'
+
+export default function Card() {
+    return (
+        <div className="card">
+            <h3>Card Title</h3>
+            <p>Description of thise card...</p>
+        </div>
+    )
+}
--- src/components/Collumn/Collumn.jsx
+++ src/components/Collumn/Collumn.jsx
@@ -1,9 +1,11 @@
 import './collumn.scss'
+import Card from '../Card/Card'
 
 export default function Collumn() {
     return (
         <div className="collumn">
             <h2>Column Title</h2>
+            <Card />
             <button>Add Card</button>
         </div>
     )
