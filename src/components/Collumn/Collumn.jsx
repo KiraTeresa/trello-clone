cc1362416fc3592443a475f90f2d0e25bbfe394b
Kira <kira.soba@gmx.de>
2023-01-04 20:56:09 +0100

    First commit

---
+++ src/components/Collumn/Collumn.jsx
@@ -0,0 +1,10 @@
+import './collumn.scss'
+
+export default function Collumn() {
+    return (
+        <div className="collumn">
+            <h2>Column Title</h2>
+            <button>Add Card</button>
+        </div>
+    )
+}
